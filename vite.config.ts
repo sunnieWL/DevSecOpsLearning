cb2b19b2e63c96a66a5ecf73a157877d9ec4223a
sunnieWL <weerapat.16141@gmail.com>
2025-11-06 09:58:59 +0700

    add this frontend to git for the first time

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react-swc';
-
-// https://vite.dev/config/
+// filepath: demo-se2-frontend/vitest.config.ts
+import { defineConfig } from 'vitest/config';
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, './src')}/`,
-    },
-  },
+ test: {
+ environment: 'jsdom',
+ globals: true,
+ },
 });
